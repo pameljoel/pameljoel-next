bb7af863ce70d7381193ca8d476ee509c78104e0
jbeltre <joel.beltre@lastminute.com>
2021-06-04 18:34:28 +0200

    added active state to navigation

--- pages/src/components/navigation/DesktopNavigation.js
+++ pages/src/components/navigation/DesktopNavigation.js
@@ -1,59 +1,62 @@
 import React from 'react';
 import Link from 'next/link'
+import {useRouter} from "next/router";
 
 import Branding from './Branding';
-import { openCrisp } from '../crisp/Crisp';
+import {openCrisp} from '../crisp/Crisp';
+
 
 const DesktopNavigation = () => {
-  return (
-    <div className="desktop-navigation">
-      <ul className="main-navigation-branding">
-        <li>
-          <Branding />
-        </li>
-      </ul>
-      <ul className="main-navigation-links">
-        <li>
-          <Link href="/" activeClassName="active">
-            <a>
-              <div className="nav-link">Home</div>
-              <div className="active-bar" />
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/daily" activeClassName="active">
-            <a>
+    const router = useRouter();
 
-            <div className="nav-link">Daily</div>
-            <div className="active-bar" />
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/curriculum" activeClassName="active">
-            <a>
-            <div className="nav-link">CV</div>
-            <div className="active-bar" />
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/projects" activeClassName="active">
-            <a>
-            <div className="nav-link">Projects</div>
-            <div className="active-bar" />
-            </a>
-          </Link>
-        </li>
-        <li>
-          <button className="nav-button" onClick={openCrisp}>
-            Contact me
-          </button>
-        </li>
-      </ul>
-    </div>
-  );
+    return (
+        <div className="desktop-navigation">
+            <ul className="main-navigation-branding">
+                <li>
+                    <Branding/>
+                </li>
+            </ul>
+            <ul className="main-navigation-links">
+                <li>
+                    <Link href="/">
+                        <a className={router.asPath == "/" ? "active" : ""}>
+                            <div className="nav-link">Home</div>
+                            <div className="active-bar"/>
+                        </a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/daily">
+                        <a className={router.asPath == "/daily" ? "active" : ""}>
+                            <div className="nav-link">Daily</div>
+                            <div className="active-bar"/>
+                        </a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/curriculum">
+                        <a className={router.asPath == "/curriculum" ? "active" : ""}>
+                            <div className="nav-link">CV</div>
+                            <div className="active-bar"/>
+                        </a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/projects">
+                        <a className={router.asPath == "/projects" ? "active" : ""}>
+                            <div className="nav-link">Projects</div>
+                            <div className="active-bar"/>
+                        </a>
+                    </Link>
+                </li>
+                <li>
+                    <button className="nav-button" onClick={openCrisp}>
+                        Contact me
+                    </button>
+                </li>
+            </ul>
+        </div>
+    );
 };
 
 export default DesktopNavigation;
--- pages/src/components/navigation/MobileNavigation.js
+++ pages/src/components/navigation/MobileNavigation.js
@@ -4,55 +4,57 @@ import Link from 'next/link'
 import {openCrisp} from '../crisp/Crisp';
 import Branding from './Branding';
 import Hamburger from './Hamburger';
+import {useRouter} from "next/router";
 
 const MobileNavigation = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
+    const router = useRouter();
+    const [menuOpen, setMenuOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setMenuOpen((prevState) => !prevState);
-  };
+    const toggleMenu = () => {
+        setMenuOpen((prevState) => !prevState);
+    };
 
-  const ListItem = (props) => {
-    const { text, to } = props;
+    const ListItem = (props) => {
+        const {text, to} = props;
+        return (
+            <li onClick={toggleMenu}>
+                <Link href={to} >
+                    <a className={router.asPath === to ? "active" : ""}>
+                        <div className="nav-link">{text}</div>
+                        <div className="active-bar"/>
+                    </a>
+                </Link>
+            </li>
+        );
+    };
+
     return (
-      <li onClick={toggleMenu}>
-        <Link href={to} activeClassName="active">
-            <a>
-                <div className="nav-link">{text}</div>
-                <div className="active-bar" />
-            </a>
-        </Link>
-      </li>
+        <Fragment>
+            <div className={`mobile-navigation ${menuOpen ? 'open' : ''}`}>
+                <Hamburger open={menuOpen} callback={toggleMenu}/>
+                <ul className="mobile-navigation-links">
+                    <ListItem to="/" text="Home"/>
+                    <ListItem to="/daily" text="Daily"/>
+                    <ListItem to="/curriculum" text="Curriculum Vitae"/>
+                    <ListItem to="/projects" text="Projects"/>
+                    <li
+                        onClick={() => {
+                            toggleMenu();
+                            openCrisp();
+                        }}
+                    >
+                        <button className="nav-button">Contact me</button>
+                    </li>
+                </ul>
+                <Branding/>
+            </div>
+            <div
+                className={`overlay-layer ${menuOpen ? 'open' : ''}`}
+                key="overlay-layer"
+                onClick={toggleMenu}
+            />
+        </Fragment>
     );
-  };
-
-  return (
-    <Fragment>
-      <div className={`mobile-navigation ${menuOpen ? 'open' : ''}`}>
-        <Hamburger open={menuOpen} callback={toggleMenu} />
-        <ul className="mobile-navigation-links">
-          <ListItem to="/" text="Home" />
-          <ListItem to="/daily" text="Daily" />
-          <ListItem to="/curriculum" text="Curriculum Vitae" />
-          <ListItem to="/projects" text="Projects" />
-          <li
-            onClick={() => {
-              toggleMenu();
-              openCrisp();
-            }}
-          >
-            <button className="nav-button">Contact me</button>
-          </li>
-        </ul>
-        <Branding />
-      </div>
-      <div
-        className={`overlay-layer ${menuOpen ? 'open' : ''}`}
-        key="overlay-layer"
-        onClick={toggleMenu}
-      />
-    </Fragment>
-  );
 };
 
 export default MobileNavigation;
