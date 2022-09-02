29deaa68ba4a562d903965b1c187031ee6419bac
jbeltre <joel.beltre@lastminute.com>
2022-09-02 15:02:31 +0200

    order css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,24 @@
-//import '../styles/globals.css'
+// external css
+import '../components/crisp/crisp.scss';
+import 'react-tippy/dist/tippy.css';
+import 'react-image-lightbox/style.css';
+import '../components/carousel/slick.scss';
 
+// global css
 import '../mixins.scss';
 import '../variables.scss';
+import '../index.scss';
+import '../App.scss';
+import '../buttons.scss';
+
+// home page
+import '../components/home/home.scss';
+
+// daily page
+import '../components/daily/daily.scss';
+import '../components/daily/modal.scss';
+
+// curriculum page
 import '../components/curriculum/curriculum.scss';
 import '../components/curriculum/career.scss';
 import '../components/curriculum/company.scss';
@@ -9,40 +26,34 @@ import '../components/curriculum/education.scss';
 import '../components/curriculum/field.scss';
 import '../components/curriculum/school.scss';
 import '../components/curriculum/section.scss';
-import '../components/footer/footer.scss';
 
-import '../components/relatedProjects/relatedProjects.scss';
+// projects page
+import '../components/projects/projects.scss';
 
-import '../components/tags/tag.scss';
-import '../components/tags/tags.scss';
-
-import '../index.scss';
-import '../App.scss';
-import '../bigheader.scss';
-import '../buttons.scss';
-import '../components/buttons/closeButton.scss';
-
-import '../components/card.scss'
-
-import '../components/crisp/crisp.scss';
-import 'react-tippy/dist/tippy.css';
-
-import '../components/skills/skills.scss'
-import '../components/skills/partials/additionalInfo.scss'
-import '../components/home/home.scss'
-import '../components/scrollTop/scrollTop.scss'
-import '../components/utils/FadeIn.scss'
+// navigation
 import '../components/navigation/navigation.scss';
 import '../components/navigation/hamburger.scss';
 import '../components/navigation/mobileNavigation.scss';
 import '../components/navigation/desktopNavigation.scss';
 
-import 'react-image-lightbox/style.css';
-import '../components/daily/daily.scss';
-import '../components/daily/modal.scss';
+// footer
+import '../components/footer/footer.scss';
 
-import '../components/projects/projects.scss';
-import '../components/carousel/slick.scss';
+// tags
+import '../components/tags/tag.scss';
+import '../components/tags/tags.scss';
+
+// skills
+import '../components/skills/skills.scss';
+import '../components/skills/partials/additionalInfo.scss';
+
+// other components
+import '../bigheader.scss';
+import '../components/relatedProjects/relatedProjects.scss';
+import '../components/buttons/closeButton.scss';
+import '../components/card.scss'
+import '../components/scrollTop/scrollTop.scss';
+import '../components/utils/FadeIn.scss';
 
 import Navigation from "../components/navigation/Navigation";
 import ScrollTop from "../components/scrollTop/ScrollTop";
